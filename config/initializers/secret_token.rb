d64ffd93fcec794e8a429a6e375f60040672788e
Salym Senyonga <salymsash@gmail.com>
2013-01-27 04:14:02 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortune::Application.config.secret_token = '220e3d8b5666e2f8b3ca872ce50d63e87a3935ffe29a37a39d22d3848520c9057098a84a4fa38aa9c56bd6eb86c0a9f456cc82bc086d8141811c6c18c80a63c8'
