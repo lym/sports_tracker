d0e23c3dcc7a5d560edb9a9891debfc49e3f99a0
Salym Senyonga <salymsash@gmail.com>
2015-11-18 09:52:29 +0300

    spec/controllers/home_controller_spec.rb: Add home controller tests
    Signed-off-by: Salym Senyonga <salymsash@gmail.com>

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,4 +9,35 @@ describe HomeController do
     end
   end
 
+	describe "GET 'legal'" do
+		it "should be successful" do
+			get 'legal'
+			response.should be_success
+		end
+	end
+
+	describe "GET 'privacy'" do
+		it "should be successful" do
+			get 'privacy'
+			response.should be_success
+		end
+	end
+
+	describe "GET 'about'" do
+		it "should be successful" do
+			get 'about'
+		end
+	end
+
+	describe "GET 'contact'" do
+		it "should be successful" do
+			get 'contact'
+		end
+	end
+
+	describe "GET 'coming_soon'" do
+		it "should be successful" do
+			get 'coming_soon'
+		end
+	end
 end
